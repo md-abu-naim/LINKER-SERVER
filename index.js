119c02835700113678f4e2620de0f38a3f204ca3
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-09 09:54:02 +0600

    deploy to vercel

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const app = express()
 const port = process.env.PORT || 5000
 
 const corsOptions = {
-    origin: ['http://localhost:3000', '']
+    origin: ['http://localhost:3000', 'https://linkerbd.vercel.app', 'https://linker-psi-ruddy.vercel.app'],
+    credentials: true,
+    optionSuccessStatus: 200
 }
-app.use(cors())
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to our LINKER!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`LINKER listening on port ${port}`)
 })
