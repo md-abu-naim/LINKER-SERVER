4cd2c35c2761e6bbbfb13463a0ca9be6153c7990
abunaim34 <farhanadnanfarabi@gmail.com>
2026-02-21 10:39:30 +0600

    get all posts by getPosts functions

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -2,9 +2,10 @@ import { getPostCollection } from "./post.model.js"
 
 
 
+// Get all posts
 export const getPosts = async(req, res) => {
     const collection = await getPostCollection()
 
-    const posts = collection.find().toArray()
+    const posts = await collection.find().toArray()
     res.send(posts)
 }
