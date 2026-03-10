50ca870020123b4ceeca480bb5a85f97d590f21a
abunaim34 <farhanadnanfarabi@gmail.com>
2026-03-10 13:50:04 +0600

    upload file to cloudinary successfully

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,12 +1,11 @@
 import multer from "multer" 
 
-
 const storage = multer.memoryStorage()
 
 const upload = multer({
     storage,
     limits:{
-        fileSize: 50 * 102 * 1024
+        fileSize: 50 * 1024 * 1024
     }
 })
 
